45d9633dcde7ff9ec2abef21e7836862163a8beb
mirzayogy <mirza.yogy@gmail.com>
2024-04-27 09:22:02 +0800

    Menggunakan Scanner

--- Main.java
+++ Main.java
@@ -1,8 +1,20 @@
 import library.Mahasiswa;
+import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
         System.out.println("Halo");
-        Mahasiswa.info();
+        // Mahasiswa.info();
+
+        double ipk = 3.9;
+        int jumlahSks = 144;
+        String nama = "Mirza";
+        System.out.println(nama);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Masukkan nama: ");
+        nama = Scanner.nextLine();
+        System.out.println(nama);
+        ipk = scanner.nextInt();
     }
 }
