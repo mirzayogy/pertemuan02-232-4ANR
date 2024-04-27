a429f00faeb030a878835eabd7e44e162f15ed53
mirzayogy <mirza.yogy@gmail.com>
2024-04-27 10:00:45 +0800

    operasi string

--- OperasiString.java
+++ OperasiString.java
@@ -3,5 +3,17 @@ public class OperasiString{
        String kota = "Banjarbaru";
 
        System.out.println(kota);
+
+       char[] uniskaChar = {'U','N','I','S','K','A'};
+       System.out.println(uniskaChar);
+
+       System.out.println(kota.toUpperCase());
+       System.out.println(kota.toLowerCase());
+       System.out.println(kota.substring(3));
+       System.out.println(kota.substring(4,8));
+       System.out.println(kota.length());
+
+
+
     }
 }
